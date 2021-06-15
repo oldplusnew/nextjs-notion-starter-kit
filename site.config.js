1b9da159c77c905ead13cdc1f6538e82e0615236
oldplusnew <cjx423@qq.com>
2021-06-15 14:24:13 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Just-Try-A-New-Blog-503d4d85de0f48d090d7bd773d6f84fd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name:  'Just Try A New Blog',
+  domain: 'https://www.notion.so/oldplusnew/Just-Try-A-New-Blog-503d4d85de0f48d090d7bd773d6f84fd',
+  author: 'Jiaxin Chen',
 
   // open graph metadata (optional)
   description: 'Example site description',
